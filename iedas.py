c33a9f7dd5f7b9ca97e20205e249bffb922e7082
Shrey <shreyshingala1@gmail.com>
2025-03-01 14:07:19 -0500

    OG

---
+++ iedas.py
@@ -0,0 +1,10 @@
+#tkinter window
+#timer on window so time based
+#window opens randomly and when you open chrome tab.
+
+
+#more then decoding brainrot, more then 
+#open cv 
+
+#sends signal to hardware to hurt user and play noise from steven he saying failuure
+#if get it right, play a noise saying good boy
